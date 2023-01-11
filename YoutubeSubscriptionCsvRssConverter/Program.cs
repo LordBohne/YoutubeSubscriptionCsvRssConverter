0165f7512a75ffd744278d76a51104e8ecb2f282
LordBohne <bohnelord@gmail.com>
2023-01-11 08:18:59 +0100

    Export OPML file

--- YoutubeSubscriptionCsvRssConverter/Program.cs
+++ YoutubeSubscriptionCsvRssConverter/Program.cs
@@ -1,21 +1,84 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Globalization;
-using AngleSharp;
+﻿using System.Globalization;
+using System.Xml;
 using CsvHelper;
 using CsvHelper.Configuration;
 
-using var streamreader = new StreamReader("subscriptions.csv");
-using var reader = new CsvReader(streamreader, new CsvConfiguration(CultureInfo.InvariantCulture)
+var file = File.Create(GetUniqueFileName());
+var xmlWriter = XmlWriter.Create(file, new XmlWriterSettings()
 {
-    Delimiter = ","
+    Indent = true
 });
-var subcriptions = reader.GetRecords<YoutubeSubscription>();
-var rssLinks = subcriptions.Select(subscription => $"https://www.youtube.com/feeds/videos.xml?channel_id={subscription.ChannelId}"!).ToList();
-Console.WriteLine(string.Join('\n', rssLinks));
+
+xmlWriter.WriteStartElement("opml");
+xmlWriter.WriteAttributeString("version", "2.0");
+xmlWriter.WriteStartElement("head");
+xmlWriter.WriteStartElement("title");
+xmlWriter.WriteString("Youtube subscriptions export to rss");
+xmlWriter.WriteEndElement();
+xmlWriter.WriteEndElement();
+xmlWriter.WriteStartElement("body");
+xmlWriter.WriteStartElement("outline");
+xmlWriter.WriteAttributeString("text","Youtube Subscriptions");
+xmlWriter.WriteAttributeString("title","Youtube Subscriptions");
+
+using var streamReader = new StreamReader("subscriptions.csv");
+using var reader = new CsvReader(streamReader, new CsvConfiguration(CultureInfo.InvariantCulture)
+{
+    Delimiter = ",",
+    PrepareHeaderForMatch = matchArgs => matchArgs.Header.Replace(" ", "").ToLowerInvariant(),
+});
+
+foreach (var subscription in reader.GetRecords<YoutubeSubscription>())
+{
+    var rssLink = $"https://www.youtube.com/feeds/videos.xml?channel_id={subscription.ChannelId}"!;
+    xmlWriter.WriteStartElement("outline");
+    xmlWriter.WriteAttributeString("title", subscription.ChannelTitle);
+    xmlWriter.WriteAttributeString("text", subscription.ChannelTitle);
+    xmlWriter.WriteAttributeString("xmlUrl", rssLink);
+    xmlWriter.WriteAttributeString("type", "rss");
+    xmlWriter.WriteAttributeString("htmlUrl", subscription.ChannelUrl);
+    xmlWriter.WriteEndElement();
+}
+xmlWriter.WriteEndElement();
+xmlWriter.WriteEndElement();
+xmlWriter.WriteEndElement();
+xmlWriter.Flush();
+
+file.Flush(true);
+file.Dispose();
+string GetUniqueFileName()
+{
+    const string fileNameFormat = "subscriptions{0}.opml";
+    var foundNonExistingFileName = false;
+    int count = 1;
+    string nonExistingFileName = "";
+    while (!foundNonExistingFileName)
+    {
+        if (count is 1 && !File.Exists(string.Format(fileNameFormat, "")))
+        {
+            foundNonExistingFileName = true;
+            nonExistingFileName = string.Format(fileNameFormat, "");
+            continue;
+        }
+
+        string fileName = string.Format(fileNameFormat, $"({count})");
+        if (!File.Exists(fileName))
+        {
+            foundNonExistingFileName = true;
+            nonExistingFileName = fileName;
+            continue;
+        }
+        
+        count++;
+    }
+
+    return nonExistingFileName;
+}
+
 public class YoutubeSubscription
 {
     public string ChannelId { get; set; }
     public string ChannelUrl { get; set; }
-    public string ChannelName { get; set; }
+    public string ChannelTitle { get; set; }
 }
+
